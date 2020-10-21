3264780dc4e5041f584fd880252004c29d5b47fb
Daniel Bemsen <danireptor7@gmail.com>
2020-10-21 04:17:57 +0100

    added 404 page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home/Home";
 import Favorites from "./Pages/Favorites/Favorites";
 import Recipe from "./Pages/Recipe/Recipe";
 import Header from "./Components/Header/Header";
 import Search from "./Pages/Search/Search";
+import NotFound from "./Pages/Home/404/NotFound";
 
 const App = () => {
   return (
@@ -27,6 +28,8 @@ const App = () => {
             <Route path="/search" component={Search} />
             <Route path="/recipe" component={Recipe} />
             <Route path="/favorites" component={Favorites} />
+            <Route path="/404" component={NotFound} />
+            <Redirect to="/404" />
           </Switch>
           <Header />
         </Router>
---
+++ src/Pages/Home/404/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./NotFound.css";
+
+const NotFound = () => {
+  return (
+    <div className="NotFound">
+      <div className="ImgDiv"></div>
+      <div className="TextOwn"></div>
+    </div>
+  );
+};
+
+export default NotFound;
