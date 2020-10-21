6e3c072c4b5aa6b0c9571ef42579e336c42af123
Daniel Bemsen <danireptor7@gmail.com>
2020-10-21 01:11:08 +0100

    commiting recipe page

--- src/Pages/Favorites/Favorites.js
+++ src/Pages/Favorites/Favorites.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import '../../App.css'
-import './Favorites.css';
+import React from "react";
+import "../../App.css";
+import "./Favorites.css";
 
 const Favorites = () => {
   return (
-    <div className="Favorites Flex">
+    <div className="Favorites">
       <h3 className="Heading"> Favorites Page</h3>
     </div>
   );
-}
+};
 
 export default Favorites;
--- src/Pages/Recipe/Recipe.js
+++ src/Pages/Recipe/Recipe.js
@@ -1,18 +1,58 @@
 import React from "react";
 import "../../App.css";
 import "./Recipe.css";
-import Title from "../../Components/Title/Title";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
 const Recipe = () => {
   return (
-    <div className="Recipe Flex">
-      <Title
-        title="What to eat?"
-        color="var(--third-color)"
-        showTitle={false}
-        showIcon={true}
-        icon={faHeart}
-      />
+    <div className="Recipe">
+      <div className="RecipeHeader">
+        <img
+          className="RecipeImage"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQ_om8bPxd814bccjOWrgDBqqBbZqlzTXQj7g&usqp=CAU"
+          alt="Pizza"
+        />
+      </div>
+      <div className="RecipeIntro">
+        <div className="Save">
+          <h2 className="RecipeTitle">Banana Pizza</h2>
+          <FontAwesomeIcon className="RecipeSave" icon={faHeart} />
+        </div>
+        <p className="SmallTitle">Diet Based</p>
+        <p className="RecipeBrief">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio deleniti
+          sunt cum at voluptatem reprehenderit ducimus nemo aperiam. Ratione
+          vero sed, tempora quo atque doloremque ducimus nam et minus labore.
+        </p>
+      </div>
+      <div className="RecipeHealth">
+        <h4 className="SmallHeading">Health and Diet Labels</h4>
+        <p className="HealthText">
+          Calories - <span> 2000</span>
+        </p>
+        <p className="HealthText">
+          Fats - <span> 2000</span>
+        </p>
+        <p className="HealthText">
+          Sugars - <span> 2000</span>
+        </p>
+        <p className="HealthText">
+          Protein - <span> 2000</span>
+        </p>
+        <p className="HealthText">
+          Energy - <span> 2000</span>
+        </p>
+      </div>
+      <div className="RecipeIngredients">
+        <h4 className="SmallHeading">Ingredients</h4>
+        <ul className="IngredientList">
+          <li className="lists">Lorem ipsum dolor sit amet.</li>
+          <li className="lists">Lorem ipsum dolor sit amet.</li>
+          <li className="lists">Lorem ipsum dolor sit amet.</li>
+          <li className="lists">Lorem ipsum dolor sit amet.</li>
+          <li className="lists">Lorem ipsum dolor sit amet.</li>
+        </ul>
+      </div>
     </div>
   );
 };
