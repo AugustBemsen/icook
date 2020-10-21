3cbe908958ab4eb9b927db140b8ae683456453cb
Daniel Bemsen <danireptor7@gmail.com>
2020-10-21 03:05:28 +0100

    commiting favourite recipe page

--- src/Pages/Favorites/Favorites.js
+++ src/Pages/Favorites/Favorites.js
@@ -5,7 +5,71 @@ import "./Favorites.css";
 const Favorites = () => {
   return (
     <div className="Favorites">
-      <h3 className="Heading"> Favorites Page</h3>
+
+      <div className="FavoritesHero">
+        <h2 className="FavoritesHeading">Saved Recipes</h2>
+        <div className="HeadingDiv">
+          <p className="FavoriteText">
+            Here are some of you Favorites we've saved for you
+          </p>
+          <hr className="Divider" />
+          <div className="TotalCounts">
+            <h1 className="counts">12</h1>
+            <p className="CountsRecipe">Recipes Saved</p>
+          </div>
+        </div>
+      </div>
+    
+      <div className="FavoritesBody">
+        <div className="RecentSaved">
+          <img
+            src="https://hips.hearstapps.com/vidthumb/images/delish-u-rice-2-1529079587.jpg"
+            alt="recent saved"
+            className="RecentImg"
+          />
+          <div className="RecentDiv">
+            <h4 className="CardTitle">Yellow Papaya</h4>
+            <p className="CardDes">zero calories</p>
+          </div>
+        </div>
+        <div className="OldSavedGroup">
+          <div className="OldSaved">
+            <img
+              src="https://hips.hearstapps.com/vidthumb/images/delish-u-rice-2-1529079587.jpg"
+              alt="recent saved"
+              className="OldImg"
+            />
+            <div className="OldDiv">
+              <h4 className="CardTitle">Yellow Papaya</h4>
+              <p className="CardDes">zero calories</p>
+            </div>
+          </div>
+        </div> <div className="OldSavedGroup">
+          <div className="OldSaved">
+            <img
+              src="https://hips.hearstapps.com/vidthumb/images/delish-u-rice-2-1529079587.jpg"
+              alt="recent saved"
+              className="OldImg"
+            />
+            <div className="OldDiv">
+              <h4 className="CardTitle">Yellow Papaya</h4>
+              <p className="CardDes">zero calories</p>
+            </div>
+          </div>
+        </div> <div className="OldSavedGroup">
+          <div className="OldSaved">
+            <img
+              src="https://hips.hearstapps.com/vidthumb/images/delish-u-rice-2-1529079587.jpg"
+              alt="recent saved"
+              className="OldImg"
+            />
+            <div className="OldDiv">
+              <h4 className="CardTitle">Yellow Papaya</h4>
+              <p className="CardDes">zero calories</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -4,12 +4,15 @@ import "./Home.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faTwitter, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <div className="Home">
       <div className="Top">
         <div className="Bottom">
-          <FontAwesomeIcon className="HomeIcon" icon={faHeart} />
+          <Link to="/favorites">
+            <FontAwesomeIcon className="HomeIcon" icon={faHeart} />
+          </Link>
           <div className="TextDiv">
             <h1 className="Heading">
               iCook Recipe <br /> App
@@ -40,8 +43,8 @@ const Home = () => {
             </div>
             <div className="mainText">
               <p className="appIntro">
-                Are you hungry and dont know what to eat or dont know
-                how to cook it? iCook gat you covered search for a recipe.
+                Are you hungry and dont know what to eat or dont know how to
+                cook it? iCook gat you covered search for a recipe.
               </p>
             </div>
           </div>
